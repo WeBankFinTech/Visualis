28e84fa44a8caf7a7247da8313d3327284a5ceb8
andyfong <fangkun1991s@163.com>
2018-08-02 15:57:22 +0800

    feat(Display): Display feature done

--- webapp/app/globalConfig.js
+++ webapp/app/globalConfig.js
@@ -28,11 +28,11 @@ export const env = envName.production
 export default {
   dev: {
     host: '/api/v3',
-    shareHost: 'http://127.0.0.1:8080/api/v3'
+    shareHost: '/share.html'
   },
   production: {
   //  host: '/api/v1',
     host: '/api/v3',
-    shareHost: '/share'
+    shareHost: '/share.html'
   }
 }
